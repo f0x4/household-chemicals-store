b7f4eb0ac8f085f7be625809af6169487652eaeb
f0x4 <70147253+f0x4@users.noreply.github.com>
2021-08-02 02:26:10 +0300

    Поздравления в файле _tapbar.scss на 20 строке

---
+++ src/components/page-blocks/_tapbar.js
@@ -0,0 +1,12 @@
+// < TapBar >
+$(document).ready( function() {
+    // Get page URL by breaking down window.location
+    var page = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
+  
+    // Search through each link, compare href to current page
+    $(".tapbar__item").each( function(){
+        if( $(this).attr("href") == page || $(this).attr("href") == "" )
+        $(this).addClass("active");
+    })
+  });
+  // < TapBar />
