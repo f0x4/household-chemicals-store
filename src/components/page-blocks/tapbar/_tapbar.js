30a37295404f41816adff6c9b0bac21cea138dbb
f0x4 <70147253+f0x4@users.noreply.github.com>
2021-10-15 21:49:37 +0300

    tapbar js delete

--- src/components/page-blocks/tapbar/_tapbar.js
+++ src/components/page-blocks/tapbar/_tapbar.js
@@ -1,12 +1,1 @@
-// < TapBar >
-$(document).ready( function() {
-    // Get page URL by breaking down window.location
-    var page = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
-  
-    // Search through each link, compare href to current page
-    $(".tapbar__item").each( function(){
-        if( $(this).attr("href") === page ||  (( page === "") && ($(this).attr("href") == "/")) )
-        $(this).addClass("active");
-    })
-  });
-  // < TapBar />
+
