df8ff3b84f2458c5cf32b03b3dcbd7dbd5c9e6d6
f0x4 <70147253+f0x4@users.noreply.github.com>
2021-10-19 16:36:35 +0300

    profile 1/4 tabs

--- src/components/bem-blocks/btn-shop-address/_btn-shop-address.js
+++ src/components/bem-blocks/btn-shop-address/_btn-shop-address.js
@@ -1,9 +1,28 @@
+function zHide() {
+    $('.header').css({"display":"none"})
+}
+function zShow() {
+    $('.header').css({"display":""})
+}
+function shopsHeight() {
+    let one = $('.btn-shop-address__content').height()
+    let two = $('.btn-shop-address__header').outerHeight(true)
+    let three = $('.btn-shop-address__info').outerHeight(true)
+    let four = $('.input-search').outerHeight(true)
+    let height = one - (two + three + four);
+    $('.shops-search-and-map__search-items').css({"maxHeight":height+"px"});
+}
 function profileShopAdress() {
 
+    zHide()
     $(".profile-tabs__item>span").addClass("z0");
 
     let popup = document.getElementById('btn-shop-address__popup')
     popup.classList.add('btn-shop-address__popup-active')
+    
+    shopsHeight()
+
+    window.addEventListener("resize", shopsHeight, false);
     
     function outOfProfileShopAdressPopup(event) {
 
@@ -11,11 +30,13 @@ function profileShopAdress() {
     	let bit = event.target.compareDocumentPosition(mainRoot);
 
     	if (bit !== 10) {
+            zShow()
             $(".profile-tabs__item>span").removeClass("z0");
             let popup = document.getElementById('btn-shop-address__popup')
             popup.classList.contains('btn-shop-address__popup-active');
             popup.classList.remove('btn-shop-address__popup-active');
             window.removeEventListener('mousedown', outOfProfileShopAdressPopup, true);
+            window.removeEventListener("resize", shopsHeight);
     	}
 
     }
@@ -25,6 +46,8 @@ function profileShopAdress() {
 }
 
 function profileShopAdressCloseBtn() {
+    zShow()
+    window.removeEventListener("resize", shopsHeight);
     $(".profile-tabs__item>span").removeClass("z0");
     let popup = document.getElementById('btn-shop-address__popup')
     popup.classList.contains('btn-shop-address__popup-active');
