1f04cc139ba070a4677973b8634710bb37636e2b
f0x4 <70147253+f0x4@users.noreply.github.com>
2021-11-11 19:46:32 +0300

    catalog fixes

---
+++ src/components/page-blocks/catalog-popup/_catalog-popup.js
@@ -0,0 +1,32 @@
+function markAllFlyoutCatalogs() {
+    let flyout = document.getElementsByClassName("flyout-catalog");
+    
+    for (let i = 0; i < flyout.length; i++) {
+        flyout[i].addEventListener("click", function() {
+            this.classList.add("active");
+            var panel = this.nextElementSibling;
+            panel.classList.add("active");
+        });
+    }
+}
+function catalogBack() {
+    let flyout = document.getElementsByClassName("flyout-catalog");
+    let wasOpen = false;
+
+    for (let i = 0; i < flyout.length; i++) {
+        if (flyout[i].matches(".active")) {
+            flyout[i].classList.remove("active");
+            var panel = flyout[i].nextElementSibling;
+            panel.classList.remove("active");
+            wasOpen = true;
+        }
+    }
+    if (!wasOpen) {
+        let cat = document.getElementById('catalog-popup');
+        cat.classList.remove('active')
+    }
+}
+ function catalogOpen() {
+    let cat = document.getElementById('catalog-popup');
+    cat.classList.add('active');
+ }
